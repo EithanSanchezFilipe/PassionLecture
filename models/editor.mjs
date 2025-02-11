c5a26a9375b9e81387261048cb94826012e32dc9
EithanSanchezFilipe <150446486+EithanSanchezFilipe@users.noreply.github.com>
2025-02-11 15:34:54 +0100

    feat . editor model

---
+++ models/editor.mjs
@@ -0,0 +1,14 @@
+const editoModel = (sequelize, DataTypes) => {
+  return sequelize.define('t_editor', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+};
+export { editoModel };
