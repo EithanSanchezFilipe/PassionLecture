aea1354f03a94f0cfd6f621e2908ed511615c16d
RyanDePina <156648185+RyanDPC@users.noreply.github.com>
2025-02-26 09:19:19 +0100

    feat(author): add controller and routes

--- routes/author.mjs
+++ routes/author.mjs
@@ -1,16 +1,14 @@
-// import { Router } from "express";
+import { Router } from "express";
+import { FindByAuthor, Create, Delete, Update } from "../controller/author.mjs";
+import { auth } from "../middleware/auth.mjs";
+const router = Router();
 
-// const router = Router();
-
-// // POST
-// router.post('/add', (req, res) => {res.send('add author routes')});
-// router.post('/:id/book', (req, res) => {res.send('add book author routes')});
-// // GET
-// router.get('/:id', (req, res) => {res.send('view profile author routes')});
-// // DELETE
-// router.delete('/:id/book/:book_id', (req, res) => {res.send('delete book author routes')});
-// // PUT
-// router.put('/:id/book/:book_id', (req, res) => {res.send('modify book author routes')});
-
- 
-// export default router;
+// POST
+router.post("/add", auth, Create);
+// DELETE
+router.delete("/delete/:id", auth, Delete);
+// PUT
+router.put("/:id", auth, Update);
+// GET
+router.get("/book", FindByAuthor);
+export default router;
