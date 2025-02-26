a46d8274154fb09ccf121284738f4df7433bb2a4
EithanSanchezFilipe <150446486+EithanSanchezFilipe@users.noreply.github.com>
2025-02-26 09:15:16 +0100

    feat : add swagger / schemas

--- server.mjs
+++ server.mjs
@@ -1,39 +1,47 @@
-import https from "https";
-import path from "path";
-import fs from "fs";
-import { createServer } from "http";
-import express from "express";
-import cookieParser from "cookie-parser";
-import sequelize from "./db/sequelize.mjs";
+import https from 'https';
+import path from 'path';
+import fs from 'fs';
+import { createServer } from 'http';
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import sequelize from './db/sequelize.mjs';
+import swaggerUi from 'swagger-ui-express';
+import { swaggerSpec } from './swagger.mjs';
 
 // Create app
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-const privateKey = fs.readFileSync(path.resolve("privkey.key"), "utf8");
+app.use(
+  '/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerSpec, { explorer: true })
+);
+
+const privateKey = fs.readFileSync(path.resolve('privkey.key'), 'utf8');
 // Import SSL certificate
 const sslcert = {
   key: privateKey,
-  cert: fs.readFileSync(path.resolve("cerificate.crt"), "utf8"),
+  cert: fs.readFileSync(path.resolve('cerificate.crt'), 'utf8'),
 };
 
 // Import routes
-import userRoute from "./routes/user.mjs";
-import bookRoute from "./routes/book.mjs";
-import categoryRoute from "./routes/category.mjs";
+import userRoute from './routes/user.mjs';
+import bookRoute from './routes/book.mjs';
+import categoryRoute from './routes/category.mjs';
 
 // import authorRoute from './routes/author.mjs';
 
 // Routes
-app.use("/user", userRoute);
-app.use("/book", bookRoute);
-app.use("/category", categoryRoute);
+app.use('/user', userRoute);
+app.use('/book', bookRoute);
+app.use('/category', categoryRoute);
 // app.use('/author', authorRoute);
 
 const httpsServer = https.createServer(sslcert, app);
 
 httpsServer.listen(8080, () => {
-  console.log("Server running on port https://localhost:8080");
+  console.log('Server running on port https://localhost:8080');
 });
 export { privateKey };
