7409353b1f9663c311a6692f7848836f0400a1b1
EithanSanchezFilipe <150446486+EithanSanchezFilipe@users.noreply.github.com>
2025-02-26 11:04:18 +0100

    typo

--- routes/category.mjs
+++ routes/category.mjs
@@ -1,14 +1,14 @@
-import { Router } from "express";
-import { Create, FindByCategory, Delete } from "../controller/category.mjs";
-import { auth } from "../middleware/auth.mjs";
+import { Router } from 'express';
+import { Create, FindByCategory, Delete } from '../controller/category.mjs';
+import { auth } from '../middleware/auth.mjs';
 
 const router = Router();
 
 // POST
-router.post("/add", auth, Create);
+router.post('/add', auth, Create);
 // GET
-router.get("/book", FindByCategory);
+router.get('/book', FindByCategory);
 //DELETE
-router.delete("/delete/:id", auth, Delete);
+router.delete('/delete/:id', auth, Delete);
 
 export default router;
