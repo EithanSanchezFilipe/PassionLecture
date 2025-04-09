f59af7af84ef34e7065745079c4402c615298f27
EithanSanchezFilipe <150446486+EithanSanchezFilipe@users.noreply.github.com>
2025-04-09 08:12:35 +0200

    fix: connect to localhost

--- app/backend/db/sequelize.mjs
+++ app/backend/db/sequelize.mjs
@@ -10,9 +10,9 @@ import { books, authors, editors, categories } from "./data-mock.mjs";
 
 // Create a new instance of Sequelize with the connection string to our database
 const sequelize = new Sequelize("db_passion_lecture", "root", "root", {
-  host: "db",
+  host: "localhost",
   dialect: "mysql",
-  port: 3306,
+  port: 6033,
   logging: false,
   define: {
     freezeTableName: true,
